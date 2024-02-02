816793fbe5b58aecb6b8c527df0b9499a721a1d8
Minal Abayasekara <darkhorseminal@gmail.com>
2024-02-03 00:33:56 +0530

    Three js Website inital commit

---
+++ server/routes/dalle.routes.js
@@ -0,0 +1,42 @@
+import express, { response } from "express";
+import * as dotenv from "dotenv";
+
+//import OpenAIApi from "openai";
+import OpenAI from "openai";
+
+dotenv.config();
+
+const router = express.Router();
+
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+const openai = new OpenAI({
+  organization: process.env.ORG_ID,
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const response = await openai.images.generate({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    const image = response.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Hello From DALL.E ROUTES" });
+});
+
+export default router;
